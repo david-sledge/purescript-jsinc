7361b222074bc2fdf637778a27df0c7d614f6f12
David M. Sledge <sledged@gmail.com>
2024-09-07 23:12:44 -0600

    getting example working...

--- examples/fetch-json/src/Main.purs
+++ examples/fetch-json/src/Main.purs
@@ -26,11 +26,11 @@ import Control.Jsinc.Decoder
   , DecodeExcption(DecodeError, ParseError, ShouldNeverHappen)
   , decodeJsonStreamT
   , decodeLastChunkT
-  , helper
+  , decodeLastChunkHelperT
   )
 import Control.Monad.Error.Class (class MonadThrow)
 import Control.Monad.Except (ExceptT, throwError)
-import Control.Monad.State.Trans (StateT)
+import Control.Monad.State.Trans (class MonadState, StateT, get)
 import Control.Monad.Reader (class MonadReader, ask, runReaderT)
 import Control.Promise as Promise
 import Data.ArrayBuffer.Typed as AB
@@ -41,6 +41,7 @@ import Data.HTTP.Method (Method(GET))
 import Data.HashMap (HashMap, empty, insert, lookup)
 import Data.Int (fromNumber)
 import Data.Maybe (Maybe(Just, Nothing), maybe)
+import Data.Show.Generic (genericShow)
 import Data.Source
   ( InPlaceSource(InPlaceSource)
   , LineColumnPosition
@@ -69,18 +70,30 @@ import Web.HTML.HTMLTableCellElement (HTMLTableCellElement)
 import Web.Streams.ReadableStream (getReader)
 import Web.Streams.Reader (read)
 
+data Prop
+  = Actor
+  | Repo
+  | Payload
+  | Author
+  | Org
+  | Commits
+
+derive instance eqProp ∷ Eq Prop
+derive instance ordProp ∷ Ord Prop
+derive instance genericProp ∷ Generic Prop _
+
+instance showProp ∷ Show Prop where
+  show = genericShow
+
 data RecsState
   = Root
-  | Recs
+  | Projects
   | Project
-  | Actor
-  | ActorProp
-  | Repo
-  | RepoProp
-  | Payload
-  | PayloadProp
-  | Name RecsState String
+  | Proper Prop Boolean
+  | Commit
+  | Streeng RecsState String
   | Index RecsState Int
+  -- | IgnoreValue
 
 derive instance eqRecsState ∷ Eq RecsState
 derive instance ordRecsState ∷ Ord RecsState
@@ -88,41 +101,39 @@ derive instance genericRecsState ∷ Generic RecsState _
 
 instance showRecsState ∷ Show RecsState where
   show Root = "Root"
-  show Recs = "Recs"
+  show Projects = "Projects"
   show Project = "Project"
-  show Actor = "Actor"
-  show ActorProp = "ActorProp"
-  show Repo = "Repo"
-  show RepoProp = "RepoProp"
-  show Payload = "Payload"
-  show PayloadProp = "PayloadProp"
-  show (Name recsState string) = "Name (" <> show recsState <> ") " <> show string
+  show (Proper prop b) = "Proper " <> show prop <> " " <> show b
+  show Commit = "Commit"
+  show (Streeng recsState string) = "Streeng (" <> show recsState <> ") " <> show string
   show (Index recsState int) = "Index (" <> show recsState <> ") " <> show int
 
 newtype TableRowStreamDecoder = TableRowStreamDecoder (Tuple (HashMap Int HTMLTableCellElement) RecsState)
 
-newStateMVal :: forall m c. Applicative m => HashMap Int HTMLTableCellElement -> RecsState -> c -> m (Tuple TableRowStreamDecoder c)
+newStateMVal ∷ ∀ m c. Applicative m ⇒ HashMap Int HTMLTableCellElement → RecsState → c → m (Tuple TableRowStreamDecoder c)
 newStateMVal hashMap acc = pure <<< Tuple (TableRowStreamDecoder $ Tuple hashMap acc)
 
-newState :: forall m a. Applicative m => HashMap Int HTMLTableCellElement -> RecsState -> m (Tuple TableRowStreamDecoder (Maybe a))
+newState ∷ ∀ m a. Applicative m ⇒ HashMap Int HTMLTableCellElement → RecsState → m (Tuple TableRowStreamDecoder (Maybe a))
 newState hashMap acc = newStateMVal hashMap acc Nothing
 
-erroneous :: forall m a e a'. MonadThrow (DecodeExcption e a') m => e -> m a
+erroneous ∷ ∀ m a e a'. MonadThrow (DecodeExcption e a') m ⇒ e → m a
 erroneous st = throwError $ DecodeError st
 
-mkCell :: forall m a e a'. MonadEffect m => MonadThrow (DecodeExcption e a') m => e -> HashMap Int HTMLTableCellElement -> RecsState -> String -> m (Tuple TableRowStreamDecoder (Maybe a))
+mkCell ∷ ∀ m a e a'. MonadEffect m ⇒ MonadThrow (DecodeExcption e a') m ⇒ e → HashMap Int HTMLTableCellElement → RecsState → String → m (Tuple TableRowStreamDecoder (Maybe a))
 mkCell st hashMap acc str =
   case acc of
-  Index acc' ndx → do
-    tdElem ← td [] [txn str]
-    newState (insert ndx tdElem hashMap) acc'
+  Index acc' ndx → if ndx >= 0
+    then do
+      tdElem ← td [] [txn str]
+      newState (insert ndx tdElem hashMap) acc'
+    else newState hashMap acc'
   _ → erroneous st
 
 instance (MonadEffect m, MonadReader Element m) ⇒ DecodeJsonStream Unit TableRowStreamDecoder TableRowStreamDecoder m where
   decodeJsonT st@(TableRowStreamDecoder (Tuple hashMap acc)) event =
     let newAcc acc' = newState hashMap acc'
         ahhh acc' str = mkCell st hashMap acc' str
-        nameStart = newAcc $ Name acc ""
+        nameStart = newAcc $ Streeng acc ""
         err = erroneous st
     in
     case event of
@@ -133,18 +144,24 @@ instance (MonadEffect m, MonadReader Element m) ⇒ DecodeJsonStream Unit TableR
     EStringStart _ →
       case acc of
       Project → nameStart
-      Actor → nameStart
-      Repo → nameStart
-      Payload → nameStart
+      Proper prop false →
+        case prop of
+        Actor → nameStart
+        Repo → nameStart
+        Payload → nameStart
+        Author → nameStart
+        Org → nameStart
+        Commits → err
+      Commit → nameStart
       Index _ _ → nameStart
       _ → err
     EString _ str →
       case acc of
-      Name acc' str' → newAcc <<< Name acc' $ str' <> str
+      Streeng acc' str' → newAcc <<< Streeng acc' $ str' <> str
       _ → err
     EStringEnd _ →
       case acc of
-      Name acc' str →
+      Streeng acc' str →
         let indexAcc = newAcc <<< Index acc' in
         case acc' of
         Project →
@@ -153,51 +170,78 @@ instance (MonadEffect m, MonadReader Element m) ⇒ DecodeJsonStream Unit TableR
           "type" → indexAcc 1
           "public" → indexAcc 15
           "created_at" → indexAcc 16
-          "actor" → newAcc ActorProp
-          "repo" → newAcc RepoProp
-          "payload" → newAcc PayloadProp
+          "actor" → newAcc $ Proper Actor true
+          "repo" → newAcc $ Proper Repo true
+          "payload" → newAcc $ Proper Payload true
+          "org" → newAcc $ Proper Org true
           _ → (log $ "unknown name: " <> str) *> (indexAcc (-1))
-        Actor →
+        Proper prop false →
+          case prop of
+          Actor →
+            case str of
+            "id" → indexAcc 2
+            "login" → indexAcc 3
+            "gravatar_id" → indexAcc 4
+            "url" → indexAcc 5
+            "avatar_url" → indexAcc 6
+            _ → (log $ "unknown name: Actor/" <> str) *> (indexAcc (-1))
+          Repo →
+            case str of
+            "id" → indexAcc 7
+            "name" → indexAcc 8
+            "url" → indexAcc 9
+            _ → (log $ "unknown name: Repo/" <> str) *> (indexAcc (-1))
+          Payload →
+            case str of
+            "ref" → indexAcc 10
+            "ref_type" → indexAcc 11
+            "master_branch" → indexAcc 12
+            "description" → indexAcc 13
+            "pusher_type" → indexAcc 14
+            "push_id" → indexAcc 17
+            "size" → indexAcc 18
+            "distinct_size" → indexAcc 19
+            "head" → indexAcc 20
+            "before" → indexAcc 21
+            "commits" → newAcc $ Proper Commits true
+            _ → (log $ "unknown name: Payload/" <> str) *> (indexAcc (-1))
+          Author →
+            case str of
+            _ → (log $ "unknown name: Payload/Commit/Author/" <> str) *> (indexAcc (-1))
+          Org →
+            case str of
+            _ → (log $ "unknown name: Org/" <> str) *> (indexAcc (-1))
+          Commits → err
+        Commit →
           case str of
-          "id" → indexAcc 2
-          "login" → indexAcc 3
-          "gravatar_id" → indexAcc 4
-          "url" → indexAcc 5
-          "avatar_url" → indexAcc 6
-          _ → (log $ "unknown name: " <> str) *> (indexAcc (-1))
-        Repo →
-          case str of
-          "id" → indexAcc 7
-          "name" → indexAcc 8
-          "url" → indexAcc 9
-          _ → (log $ "unknown name: " <> str) *> (indexAcc (-1))
-        Payload →
-          case str of
-          "ref" → indexAcc 10
-          "ref_type" → indexAcc 11
-          "master_branch" → indexAcc 12
-          "description" → indexAcc 13
-          "pusher_type" → indexAcc 14
-          _ → (log $ "unknown name: " <> str) *> (indexAcc (-1))
-        Index acc'' ndx → do
-          tdElem ← td [] [txn str]
-          newState (insert ndx tdElem hashMap) acc''
+          "author" → newAcc $ Proper Author true
+          _ → (log $ "unknown name: Payload/Commit/" <> str) *> (indexAcc (-1))
+        Index _ _ → mkCell st hashMap acc' str
         _ → err
       _ → err
     EArrayStart →
       case acc of
-      Root → newAcc Recs
+      Root → newAcc Projects
+      Proper Commits true → newAcc $ Proper Commits false
       _ → err
     EArrayEnd →
       case acc of
-      Recs → newStateMVal hashMap Root $ pure unit
+      Projects → newStateMVal hashMap Root $ pure unit
+      Proper Commits false → newAcc $ Proper Payload false
       _ → err
     EObjectStart →
       case acc of
-      Recs → newAcc Project
-      ActorProp → newAcc Actor
-      RepoProp → newAcc Repo
-      PayloadProp → newAcc Payload
+      Projects → newAcc Project
+      Proper prop true →
+        let newAccValue = newAcc $ Proper prop false in
+        case prop of
+        Actor → newAccValue
+        Repo → newAccValue
+        Payload → newAccValue
+        Author → newAccValue
+        Org → newAccValue
+        Commits → err
+      Proper Commits false → newAcc Commit
       _ → err
     EObjectEnd →
       case acc of
@@ -209,10 +253,16 @@ instance (MonadEffect m, MonadReader Element m) ⇒ DecodeJsonStream Unit TableR
         buildRow 0
         tbody ← ask
         tbody +< [trElem # nd] # void
-        newState empty Recs
-      Actor → newAcc Project
-      Repo → newAcc Project
-      Payload → newAcc Project
+        newState empty Projects
+      Proper prop false →
+        case prop of
+        Actor → newAcc Project
+        Repo → newAcc Project
+        Payload → newAcc Project
+        Org → newAcc Project
+        Author → newAcc Commit
+        Commits → err
+      Commit → newAcc $ Proper Commits false
       _ → err
     EJsonEnd → err
 
@@ -227,10 +277,10 @@ instance (MonadEffect m, MonadReader Element m) ⇒ DecodeJsonStream Unit TableR
         _ → err
       _ → err
 
-whuh :: forall s c a p s'. s' -> Tuple (Tuple a (SourcePosition s p)) c -> Tuple (Tuple a (SourcePosition (InPlaceSource s') p)) c
+whuh ∷ ∀ s c a p s'. s' → Tuple (Tuple a (SourcePosition s p)) c → Tuple (Tuple a (SourcePosition (InPlaceSource s') p)) c
 whuh jsonStr (Tuple (Tuple parseState (SourcePosition _ position)) acc) = Tuple (Tuple parseState (SourcePosition (InPlaceSource jsonStr 0) position)) acc
 
-letsBegin :: Tuple (Tuple ParseState (SourcePosition (InPlaceSource String) LineColumnPosition)) TableRowStreamDecoder
+letsBegin ∷ Tuple (Tuple ParseState (SourcePosition (InPlaceSource String) LineColumnPosition)) TableRowStreamDecoder
 letsBegin = Tuple (Tuple initParseState (initStringPosition "")) (TableRowStreamDecoder $ Tuple empty Root)
 
 decodeJsonResponseStream ∷ ∀ a c e m.
@@ -238,21 +288,20 @@ decodeJsonResponseStream ∷ ∀ a c e m.
   MonadEffect m ⇒
   MonadReader Element m ⇒
   MonadAff m ⇒
-  Response ->
-  c ->
+  Response →
+  c →
   m (Tuple (Tuple (Maybe a) (Maybe (DecodeExcption e a))) (Tuple (Tuple ParseState (SourcePosition (InPlaceSource String) LineColumnPosition)) c))
 decodeJsonResponseStream response startAcc = do
   reader ← liftEffect $ body response >>= getReader
   decoder ← liftEffect $ new utf8
   let stream chunkEndF eofF mA aF state = do
         log "Getting next chunk..."
-        mChunk <- liftAff (Promise.toAffE (unsafeCoerce $ read reader))
+        mChunk ← liftAff (Promise.toAffE (unsafeCoerce $ read reader))
         maybe (do
             log "No next chunk..."
             chunk ∷ ArrayView Uint8 ← liftEffect $ AB.empty 0
             -- empty out any remaining encoded data in the decoder's buffer
             jsonStr ← liftEffect $ decodeWithOptions chunk {stream: false} decoder
-            log jsonStr
             chunkEndF jsonStr
           )
           (\ chunk → do
@@ -270,20 +319,20 @@ decodeJsonResponseStream response startAcc = do
           )
           mChunk
       checkRestOfStream a state = stream
-        (\ jsonStr -> do
-            Tuple (Tuple mA e) state' ← helper a $ whuh jsonStr state
+        (\ jsonStr → do
+            Tuple (Tuple mA e) state' ← decodeLastChunkHelperT a $ whuh jsonStr state
             pure $ Tuple (Tuple mA
                 case e of
-                ParseError EOF -> Nothing
-                _ -> Just e
+                ParseError EOF → Nothing
+                _ → Just e
               ) state'
         )
         (checkRestOfStream a)
         (Just a)
-        (\ a' -> pure <<< Tuple (Tuple (Just a) <<< Just <<< ShouldNeverHappen $ Just a'))
+        (\ a' → pure <<< Tuple (Tuple (Just a) <<< Just <<< ShouldNeverHappen $ Just a'))
         state
       decodeNextChunk state = stream
-        (\ jsonStr -> decodeLastChunkT Nothing $ whuh jsonStr state)
+        (\ jsonStr → decodeLastChunkT Nothing $ whuh jsonStr state)
         decodeNextChunk
         Nothing
         checkRestOfStream
@@ -329,15 +378,32 @@ main = do
         , duplex: Half
         }
       response ← Promise.toAffE $ unsafeCoerce $ Fetch.fetch request
-      Tuple (Tuple (mA :: Maybe Unit) mE) (Tuple (Tuple parseState (SourcePosition source pos)) _) <- runReaderT (decodeJsonResponseStream response (TableRowStreamDecoder $ Tuple empty Root)) tableBody
-      maybe (pure unit) (\ e -> case e of
-          ParseError err -> log $ show err
-          ShouldNeverHappen mA' -> log $ "Something happened that shouldn't have: " <> show mA <> " and " <> show mA'
-          DecodeError e' -> do
-            log $ show parseState
-            log $ show pos
-            log $ show source
-            case e' of
-              TableRowStreamDecoder (Tuple _ recsState) -> log $ show recsState
+      -- reader ← liftEffect $ body response >>= getReader
+      -- decoder ← liftEffect $ new utf8
+      -- let stream = do
+      --       log "Getting next chunk..."
+      --       mChunk ← liftAff (Promise.toAffE (unsafeCoerce $ read reader))
+      --       maybe (do
+      --           log "No next chunk..."
+      --           chunk ∷ ArrayView Uint8 ← liftEffect $ AB.empty 0
+      --           -- empty out any remaining encoded data in the decoder's buffer
+      --           jsonStr ← liftEffect $ decodeWithOptions chunk {stream: false} decoder
+      --           log jsonStr
+      --         )
+      --         (\ chunk → do
+      --           log <<< show $ AB.length chunk
+      --           jsonStr ← liftEffect $ decodeWithOptions chunk {stream: true} decoder
+      --           log jsonStr
+      --           stream
+      --         )
+      --         mChunk
+      -- stream
+      Tuple (Tuple mA mE) (Tuple state _) ← runReaderT (decodeJsonResponseStream response (TableRowStreamDecoder $ Tuple empty Root)) tableBody
+      maybe (maybe (log "No error, but no indication of success either...") pure mA) (\ e → do
+          log $ show state
+          case e of
+            ParseError err → log $ show err
+            ShouldNeverHappen mA' → log $ "Something happened that shouldn't have: " <> show mA <> " and " <> show mA'
+            DecodeError (TableRowStreamDecoder (Tuple _ recsState)) → log $ show recsState
         ) mE
       log $ "response.status: " <> show (status response)
--- src/Control/Jsinc/Decoder.purs
+++ src/Control/Jsinc/Decoder.purs
@@ -7,7 +7,7 @@ module Control.Jsinc.Decoder
   , decodeT
   , endJsonDecodeT
   , endJsonStreamDecodeT
-  , helper
+  , decodeLastChunkHelperT
   )
   where
 
@@ -26,7 +26,7 @@ import Control.Monad.Error.Class (class MonadThrow)
 import Control.Monad.Except (ExceptT, throwError)
 import Control.Monad.Maybe.Trans (MaybeT)
 import Control.Monad.State (class MonadState, StateT, get, modify, put)
-import Data.Either (either)
+import Data.Either (Either, either)
 import Data.Maybe (Maybe(Just, Nothing), maybe)
 import Data.Source (class Source)
 import Data.Tuple (Tuple(Tuple))
@@ -48,76 +48,74 @@ class DecodeJsonStream a e c m where
     Event →
     m (Tuple c (Maybe (Maybe a)))
 
+processJsonStreamT ∷ ∀ m a s c ev e a'.
+  MonadState (Tuple s c) m ⇒
+  MonadThrow (DecodeExcption e a') m ⇒
+  (s → m (Tuple (Either ParseException ev) s)) →
+  (c → ev → m (Tuple c (Maybe a))) →
+  m a
+processJsonStreamT f g =
+  let recurse = do
+        Tuple state acc ← get
+        Tuple result state' ← f state
+        put (Tuple state' acc)
+        either (throwError <<< ParseError) (\ event → do
+            (Tuple acc' mRes) ← g acc event
+            modify (\ (Tuple state'' _) → Tuple state'' acc') *> maybe recurse pure mRes
+          ) result
+  in
+  recurse
+
 decodeJsonStreamT ∷ ∀ s a e c m.
   MonadState (Tuple (Tuple ParseState s) c) m ⇒
   MonadThrow (DecodeExcption e a) m ⇒
   Source s Char (MaybeT m) ⇒
   DecodeJsonStream a e c m ⇒
   m a
-decodeJsonStreamT =
-  let recurse = do
-        Tuple state acc ← get
-        Tuple result state' ← parseJsonStreamT state
-        put (Tuple state' acc)
-        either (throwError <<< ParseError) (\ event → do
-            (Tuple acc' mRes) ← decodeJsonT acc event
-            modify (\ (Tuple state'' _) → Tuple state'' acc') *> maybe recurse pure mRes
-          ) result
-  in
-  recurse
+decodeJsonStreamT = processJsonStreamT parseJsonStreamT decodeJsonT
 
 endJsonStreamDecodeT ∷ ∀ s a e c m.
   MonadState (Tuple (Tuple ParseState s) c) m ⇒
   MonadThrow (DecodeExcption e a) m ⇒
   DecodeJsonStream a e c m ⇒
   m (Maybe a)
-endJsonStreamDecodeT =
-  let recurse = do
-        Tuple state acc ← get
-        Tuple result state' ← endJsonStreamParseT state
-        put (Tuple state' acc)
-        either (throwError <<< ParseError) (\ event → do
-            (Tuple acc' mRes) ← endJsonDecodeT acc event
-            modify (\ (Tuple state'' _) → Tuple state'' acc') *> maybe recurse pure mRes
-          ) result
-  in
-  recurse
+endJsonStreamDecodeT = processJsonStreamT endJsonStreamParseT endJsonDecodeT
 
-helper ∷ forall m s c a e.
+decodeLastChunkHelperT ∷ ∀ m s c a e.
   DecodeJsonStream a e c (ExceptT (DecodeExcption e a) (StateT (Tuple (Tuple ParseState s) c) m)) ⇒
   Monad m ⇒
   Source s Char (MaybeT (ExceptT (DecodeExcption e a) (StateT (Tuple (Tuple ParseState s) c) m))) ⇒
   a →
   Tuple (Tuple ParseState s) c →
   m (Tuple (Tuple (Maybe a) (DecodeExcption e a)) (Tuple (Tuple ParseState s) c))
-helper a state = do
+decodeLastChunkHelperT a state = do
   -- When a value has already been supllied, the next call...
   Tuple result state' ← runParseT decodeJsonStreamT state
   pure $ Tuple (Tuple (Just a) $ either
-    -- ... should return an exception...
-    identity
-    -- ... but not a value.
-    (ShouldNeverHappen <<< Just)
-    result) state'
+      -- ... should return an exception...
+      identity
+      -- ... but not a value.
+      (ShouldNeverHappen <<< Just)
+      result) state'
 
 -- | Finish decoding a JSON string.
-decodeLastChunkT ∷ forall m s c a e.
+decodeLastChunkT ∷ ∀ m s c a e.
   DecodeJsonStream a e c (ExceptT (DecodeExcption e a) (StateT (Tuple (Tuple ParseState s) c) m)) ⇒
   Monad m ⇒
   Source s Char (MaybeT (ExceptT (DecodeExcption e a) (StateT (Tuple (Tuple ParseState s) c) m))) ⇒
   Maybe a →
   Tuple (Tuple ParseState s) c →
   m (Tuple (Tuple (Maybe a) (Maybe (DecodeExcption e a))) (Tuple (Tuple ParseState s) c))
-decodeLastChunkT mA state_ = do
+decodeLastChunkT mA state = do
   Tuple (Tuple mA' e') state'' ← maybe
     (do
-      Tuple result state ← runParseT decodeJsonStreamT state_
+      Tuple result state' ← runParseT decodeJsonStreamT state
       either
-        (\ e → pure $ Tuple (Tuple Nothing e) state)
-        (\ a → helper a state
+        (\ e → pure $ Tuple (Tuple Nothing e) state')
+        (\ a → decodeLastChunkHelperT a state'
         ) result
     )
-    (\ a -> helper a state_)
+    (\ a → decodeLastChunkHelperT a state)
     mA
   case e' of
     ParseError EOF → maybe
@@ -125,7 +123,7 @@ decodeLastChunkT mA state_ = do
         Tuple result state''' ← runParseT endJsonStreamDecodeT state''
         pure $ (either
             (\ e'' → Tuple (Tuple mA' $ Just e''))
-            (\ mA'' -> maybe
+            (\ mA'' → maybe
               (Tuple $ Tuple mA' <<< Just $ ShouldNeverHappen Nothing)
               (const $ Tuple (Tuple mA'' Nothing))
               mA''
@@ -137,7 +135,7 @@ decodeLastChunkT mA state_ = do
     _ → pure $ Tuple (Tuple mA' $ pure e') state''
 
 -- | Decode a JSON string as a single chunk.
-decodeT ∷ forall m s c a e.
+decodeT ∷ ∀ m s c a e.
   DecodeJsonStream a e c (ExceptT (DecodeExcption e a) (StateT (Tuple (Tuple ParseState s) c) m)) ⇒
   Monad m ⇒
   Source s Char (MaybeT (ExceptT (DecodeExcption e a) (StateT (Tuple (Tuple ParseState s) c) m))) ⇒
